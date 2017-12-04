ab1bce2df6d8247de38aa7c658cf8bc4a70198db
Nick Shvelidze <nick.shvelidze@azry.com>
2017-12-04 13:13:49 +0400

    Zprezto

---
+++ setup.zsh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+# Use zsh as default shell
+chsh -s /bin/zsh
+
+# Setup Zprezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# Zprezto overrides
+for rcfile in "$PWD"/zprezto; do
+  ln -fs "$rcfile" "$HOME/.${rcfile:t}"
+done
